1f5fe83fc9436c16417dda86491fe73d7bd66c04
Ryan Cheung <ryanzjlib@gmail.com>
2014-11-23 09:41:28 +0800

    PS1

---
+++ Data Wrangling with MongoDB/Lesson1/PS1.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+"""
+Your task is to process the supplied file and use the csv module to extract data from it.
+The data comes from NREL (National Renewable Energy Laboratory) website. Each file
+contains information from one meteorological station, in particular - about amount of
+solar and wind energy for each hour of day.
+
+Note that the first line of the datafile is neither data entry, nor header. It is a line
+describing the data source. You should extract the name of the station from it.
+
+The data should be returned as a list of lists (not dictionaries).
+You can use the csv modules "reader" method to get data in such format.
+Another useful method is next() - to get the next line from the iterator.
+You should only change the parse_file function.
+"""
+import csv
+import os
+
+DATADIR = os.getcwd()
+DATAFILE = "745090.csv"
+
+
+def parse_file(datafile):
+    name = ""
+    data = []
+    with open(datafile,'rb') as f:
+        reader = csv.reader(f)
+        name = reader.next()[1]
+        header = list(reader.next())
+        data = [list(row) for row in reader]
+            
+    # Do not change the line below
+    return (name, data)
+
+
+def test():
+    datafile = os.path.join(DATADIR, DATAFILE)
+    name, data = parse_file(datafile)
+
+    assert name == "MOUNTAIN VIEW MOFFETT FLD NAS"
+    assert data[0][1] == "01:00"
+    assert data[2][0] == "01/01/2005"
+    assert data[2][5] == "2"
+
+
+if __name__ == "__main__":
+    test()
