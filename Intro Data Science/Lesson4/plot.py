2cd5ebb3b9819065661fb1d5b183356e759d4972
Ryan Cheung <ryanzjlib@gmail.com>
2014-12-09 22:53:06 +0800

    L4

---
+++ Intro Data Science/Lesson4/plot.py
@@ -0,0 +1,27 @@
+from pandas import *
+from ggplot import *
+
+import pandas
+
+def lineplot(hr_year_csv):
+    # A csv file will be passed in as an argument which
+    # contains two columns -- 'HR' (the number of homerun hits)
+    # and 'yearID' (the year in which the homeruns were hit).
+    #
+    # Fill out the body of this function, lineplot, to use the
+    # passed-in csv file, hr_year.csv, and create a
+    # chart with points connected by lines, both colored 'red',
+    # showing the number of HR by year.
+    #
+    # You will want to first load the csv file into a pandas dataframe
+    # and use the pandas dataframe along with ggplot to create your visualization
+    #
+    # You can check out the data in the csv file at the link below:
+    # https://www.dropbox.com/s/awgdal71hc1u06d/hr_year.csv
+    #
+    # You can read more about ggplot at the following link:
+    # https://github.com/yhat/ggplot/
+    
+    
+    gg = #YOUR CODE GOES HERE
+    return gg
