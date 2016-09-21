fbe9d768c7a8332a623628f714d40046efcb304b
ryancheunggit <ryanzjlib@gmail.com>
2016-09-21 10:46:29 -0400

    some aged android stuff

---
+++ Android/playWithMapObject/app/src/main/java/info/ryanzhang/playwithmapobject/MainActivity.java
@@ -0,0 +1,67 @@
+package info.ryanzhang.playwithmapobject;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.CameraPosition;
+import com.google.android.gms.maps.model.LatLng;
+
+public class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
+    GoogleMap m_map;
+    boolean mapReady=false;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button btnMap = (Button) findViewById(R.id.btnMap);
+        btnMap.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (mapReady)
+                    m_map.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+            }
+        });
+
+        Button btnSatellite = (Button) findViewById(R.id.btnSatellite);
+        btnSatellite.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(mapReady)
+                    m_map.setMapType(GoogleMap.MAP_TYPE_SATELLITE);
+            }
+        });
+
+        Button btnHybrid = (Button) findViewById(R.id.btnHybrid);
+        btnHybrid.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (mapReady)
+                    m_map.setMapType(GoogleMap.MAP_TYPE_HYBRID);
+            }
+        });
+
+        MapFragment mapFragment = (MapFragment) getFragmentManager().findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+
+
+    }
+
+    @Override
+    public void onMapReady(GoogleMap map) {
+        mapReady=true;
+        m_map = map;
+        LatLng newYork = new LatLng(40.7484,-73.9857);
+        CameraPosition target = CameraPosition.builder().target(newYork).zoom(14).build();
+        m_map.moveCamera(CameraUpdateFactory.newCameraPosition(target));
+
+    }
+}
